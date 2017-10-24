d79a52898f22b90d25bc6d896cd99a11a7fe1e3f
Hristina Cankova <hristinacankova@gmail.com>
2017-10-24 18:23:51 +0300

    style changes

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -7,33 +7,33 @@ class Header extends React.Component{
     constructor() {
         super()
         this.onLogoutClick = this.onLogoutClick.bind(this)
-        this.onOrderClick = this.onOrderClick.bind(this)
-        this.onCuttingClick = this.onCuttingClick.bind(this)
+//        this.onOrderClick = this.onOrderClick.bind(this)
+//        this.onCuttingClick = this.onCuttingClick.bind(this)
     }
     onLogoutClick() {
         this.props.history.push('/')
         deleteToken()
     }
-    onOrderClick() {
-        this.props.history.push('/orders')
-    }
-    onCuttingClick() {
-        this.props.history.push('/cutting')
-    }
+//    onOrderClick() {
+//        this.props.history.push('/orders')
+//    }
+//    onCuttingClick() {
+//        this.props.history.push('/cutting')
+//    }
     render() {
         return(
-                <div className="container-fluid header">
+                <nav className="navbar navbar-dark bg-dark">
                     <div className="row">
                         <div className="col-md-6 col-sm-4 links">
-                            <button className="btn btn-default" onClick={this.onOrderClick}>Процеси и подов план</button>
-                            <button className="btn btn-default" onClick ={this.onCuttingClick}>Кроялна</button>
+                            <a className="nav-link" href="/orders">Процеси и подов план</a>
+                            <a className="nav-link" href="/cutting">Кроялна</a>
                         </div>
                         <div className="col-md-2 col-sm-2 logout">
                             <button className="btn btn-danger exit" onClick={this.onLogoutClick}>Изход</button>
                         </div>
                     </div>
                     
-                </div>
+                </nav>
                 
        
         )
