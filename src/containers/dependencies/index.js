2871c520ab20a14687ae517841f37c439d77a0eb
Boyan Dzambazov <boyan.dzambazov@gmail.com>
2017-11-02 16:52:39 +0200

    create floor plain component

--- src/containers/dependencies/index.js
+++ src/containers/dependencies/index.js
@@ -171,7 +171,7 @@ class Dependencies extends React.Component {
         </td>
         <td className="tech2"></td>
         <td className="tech3"></td>
-        <td className="size-180 no-border">{active && <a href="/departments/:id/plan" className="btn btn-warning showPlan">Покажи подов план</a>}</td>
+        <td className="size-180 no-border">{active && <a href={"/departments/" + rowData.id + "/plan"} className="btn btn-warning showPlan">Покажи подов план</a>}</td>
       </tr>
     )
   }
