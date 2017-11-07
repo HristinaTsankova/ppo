21fa5d2ce306588e137e3b8852bbb074a8bc310b
Boyan Dzambazov <boyan.dzambazov@gmail.com>
2017-11-07 18:34:40 +0200

    add data to a machine

--- src/containers/plan/machine_table.js
+++ src/containers/plan/machine_table.js
@@ -4,30 +4,39 @@ import { Droppable } from 'react-drag-and-drop';
 
 
 export default class MachineTable extends React.Component {
-    render() {
-        return(
-           
-            <Droppable 
-                type={['plan']}
-                onDrop={this.onDrop}>
-                <table className="table table-bordered">
-                    <tbody>
-                        <tr>
-                            <td className="floor_plan2"></td>
-                            <td className="floor_plan2"></td>
-                        </tr>
-                        <tr>
-                           <td className="floor_plan"></td>
-                           <td className="floor_plan"></td> 
-                           <td className="floor_plan"></td> 
-                           <td className="floor_plan"></td>     
-                           <td className="floor_plan"></td>   
-                           <td className="floor_plan"></td>       
-                        </tr>
-                    </tbody>
-                </table>
-            </Droppable>
-        
-        )
-    }
+  renderProcess(process, idx) {
+    return (
+      <tr key={idx + process.toString()}>
+        <td className="floor_plan">{process}</td>
+        <td className="floor_plan"></td>
+        <td className="floor_plan"></td>
+        <td className="floor_plan"></td>
+        <td className="floor_plan"></td>
+        <td className="floor_plan"></td>
+      </tr>
+    )
+  }
+
+  render() {
+    return (
+      <Droppable
+        type={['plan']}
+        onDrop={this.onDrop}>
+        <div className="margin-top-10">
+          <table className="table table-bordered">
+            <tbody>
+              <tr>
+                <td className="floor_plan2">{this.props.spot.user}</td>
+                <td className="floor_plan2"></td>
+              </tr>
+              {this.props.spot.processes.map((process, idx) => {
+                return this.renderProcess(process, idx)
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Droppable>
+
+    )
+  }
 }
