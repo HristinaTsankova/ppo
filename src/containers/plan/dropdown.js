759df181c1ea6b8011b0284bf43b77b2c8820aa8
Hristina Cankova <hristinacankova@gmail.com>
2017-11-15 15:29:22 +0200

    orders and processes

--- src/containers/plan/dropdown.js
+++ src/containers/plan/dropdown.js
@@ -1,38 +1,26 @@
 import React from 'react';
-import { sortBy } from 'lodash';
+import { connect } from 'react-redux';
 
-export default class DropDown extends React.Component {
+class DropDown extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      orders: ''
+      getOrders: ''
     }
   }
-  fetchOrders() {
-    const request = {
-      method: 'GET',
-      headers: ({ 'Accept': 'application/vnd.elitex-v1+json', 'Content-Type': 'application/json', 'Authorization': "access_token=-fKJ0-fsGTCwNcyDg1BMUQ" })
-    };
-    fetch(`http://178.62.112.203/api/fp/orders`, request).then((response) => {
-      return response.json()
-    }).then((json) => {
-      this.setState({ orders: json })
+  
 
-    });
-  }
-  componentDidMount() {
-    this.fetchOrders();
-  }
 
   renderRow(rowData, i) {
     return (
-      <option key={i} value={rowData.id}>{rowData.name}</option>
+      <option key={i} value={rowData.id}>{rowData.id}</option>
     )
   }
+  
+  
   render() {
-    const orders = sortBy(this.state.orders, 'id')
-    const rows = orders.map(this.renderRow)
+    const rows = this.props.department.orders.map(this.renderRow)
     return (
       <select className="selectpicker">
         <option>Избери модел</option>
@@ -41,3 +29,15 @@ export default class DropDown extends React.Component {
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  department: state.departments.department
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DropDown);
--- src/containers/plan/process.js
+++ src/containers/plan/process.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
-export default class Process extends React.Component{
+class Process extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            processes: ''
+        }
+    }
+
+
+    renderRow(rowData, i) {
+        return (
+          <tr key={i}>
+              <td>{rowData.serial_number}</td>
+              <td>{rowData.name}</td>
+              <td>{rowData.aligned_time}</td>
+          </tr>
+        )
+    }
+
     render(){
+        const rows = this.props.orders.order.order_processes.map(this.renderRow)
         return(
             <div>
-                <div className="row">
-                    <input className="form-control search"/>
-                </div>
+                <table className="table">
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </table>
             </div>
         )
     }
     
 }
+const mapStateToProps = (state) => ({
+    department: state.query.order,
+    orders: state.orders
+    
+  });
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      dispatch
+    };
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Process);
