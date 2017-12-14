d4e6793c36c8b8e7b819780d3372c118b37e69da
Hristina Cankova <hristinacankova@gmail.com>
2017-12-14 18:08:31 +0200

    info window

--- src/containers/plan/more.js
+++ src/containers/plan/more.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setQueryValue} from '../../actions/query';
+import { loadOrderById } from '../../actions/orders';
 
-export default class More extends React.Component {
+class More extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      earnings: [],
+      orderInfo: {
+        articles_number: 0,
+        articles_number_from_batches: 0
+      }
+    }
+  }
+  
+  componentWillReceiveProps(props) {
+    let earnings = [];
+    let orderInfo = this.state.orderInfo;
+
+    if (props.earnings !== undefined) {
+      const order = props.earnings.find((o) => o.order_id);
+      if (order !== undefined) {
+        if (order.order_info !== undefined) {
+          orderInfo = order.order_info;
+        }
+        for (const key in order.users_earnings) {
+          const element = order.users_earnings[key];
+          earnings = earnings.concat(element.earnings);
+        }
+      }
+    }
+
+    this.setState({ earnings: earnings });
+    this.setState({ orderInfo: orderInfo });
+  }
+  
+  
   render() {
+    
     return (
       <div className="well info-wrapper">
         <table className="table">
@@ -16,7 +53,7 @@ export default class More extends React.Component {
             </tr>
             <tr>
               <td>Общо скроени бройки</td>
-              <td></td>
+              <td>{this.state.orderInfo.articles_number_from_batches}</td>
               <td>Остават за кроене</td>
               <td></td>
               <td>Брой готови облекла</td>
@@ -29,9 +66,9 @@ export default class More extends React.Component {
             </tr>
             <tr>
               <td>Присъстващи</td>
-              <td></td>
+              <td>{this.props.floor.payload.currentPeople}</td>
               <td>Отсъстващи</td>
-              <td></td>
+              <td>{this.props.floor.payload.missingPeople}</td>
               <td>Работници от друга бригада</td>
               <td></td>
             </tr>
@@ -41,3 +78,22 @@ export default class More extends React.Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    floor: state.floor.floor,
+    queryOrder: state.query.order,
+    earnings: state.earnings.data,
+    order: state.orders.order,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatch,
+    setQueryValue: (val, type, callback) => dispatch(setQueryValue(val, type, callback)),
+    loadOrderData: (id) => dispatch(loadOrderById(id))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(More);
